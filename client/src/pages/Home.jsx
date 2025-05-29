b85f19ed726d9324ae383a9e82e57f5588c2e6d7
RytAakarsh <aakarshmishra70@gmail.com>
2025-05-29 15:02:46 +0530

    Frontend Files

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero.jsx'
 import FeaturedDestination from '../components/FeaturedDestination.jsx'
 import ExclusiveOffer from '../components/ExclusiveOffer.jsx'
+import Testimonial from '../components/Testimonial.jsx'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <FeaturedDestination />
       <ExclusiveOffer />
+      <Testimonial /> 
     </>
   )
 }
