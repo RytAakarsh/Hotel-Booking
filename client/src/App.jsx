e01d5afbbdbe469ce13543509d7a3f362bf6b011
RytAakarsh <aakarshmishra70@gmail.com>
2025-06-01 13:34:31 +0530

    Frontend Files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
-import { Route,Routes, useLocation } from 'react-router-dom'
-import Home from './pages/Home.jsx';
-import Footer from './components/Footer.jsx';
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Footer from './components/Footer.jsx'
+import AllRooms from './pages/AllRooms.jsx'
 
 const App = () => {
-
   const isOwnerPath = useLocation().pathname.includes("owner");
 
   return (
-    <div>
-      { !isOwnerPath && <Navbar />}
-      <div className='min-h[70vh]'>
+    <div className="min-h-screen flex flex-col">
+      {!isOwnerPath && <Navbar />}
+
+      <main className="flex-1">
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/rooms' element={<AllRooms />} />
         </Routes>
-      </div>
-      <Footer /> 
+      </main>
+
+      {!isOwnerPath && <Footer />}
     </div>
   )
 }
