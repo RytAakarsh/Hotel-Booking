d842f09005c83455a4c4ed7b901d719bc22d4a75
RytAakarsh <aakarshmishra70@gmail.com>
2025-06-19 23:48:15 +0530

    Backend File

---
+++ server/controllers/clerkWebhooks.js
@@ -0,0 +1,3 @@
+import User from "../models/user.js";
+import {Webhook} from "svix"
+
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors"
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
 
 
 connectDB()
@@ -11,9 +12,18 @@ const app = express()
 
 app.use(cors())
 
+// MiddleWare
+app.use(express.json())
+app.use(clerkMiddleware())
+
+
+
 app.get('/', (req,res) => res.send("API is Working"))
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+  
 
