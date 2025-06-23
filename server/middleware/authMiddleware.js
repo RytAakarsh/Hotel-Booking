32778592ffa80acfae623eda46fbfe734d914496
RytAakarsh <aakarshmishra70@gmail.com>
2025-06-23 12:52:01 +0530

    backend

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,29 +1,14 @@
 import  User from "../models/User.js"
 
-//middleware to check if user is authenticated 
 
-// export const protect = async ( requestAnimationFrame, res , next ) => {
-//     const {userId} = requestAnimationFrame.auth;
-//     if(!userId) {
-//         res.json({success: false, message: "not authenticated"})
-//     }else {
-//         const user = await User.findById(userId);
-//         req.user = user;
-//         next()
-//     }
-// }
-// middleware/authMiddleware.js
-export const protect = async (req, res, next) => {
-  try {
-    const { userId } = req.auth;
 
-    if (!userId) {
-      return res.status(401).json({ success: false, message: "Not authenticated" });
+export const protect = async ( requestAnimationFrame, res , next ) => {
+    const {userId} = requestAnimationFrame.auth;
+    if(!userId) {
+        res.json({success: false, message: "not authenticated"})
+    }else {
+        const user = await User.findById(userId);
+        req.user = user;
+        next()
     }
-
-    req.user = { _id: userId };
-    next();
-  } catch (error) {
-    res.status(401).json({ success: false, message: "Authentication failed" });
-  }
-};
+}
